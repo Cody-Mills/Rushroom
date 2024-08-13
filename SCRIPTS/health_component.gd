2703b6f82642a03278d5304fd98676eafc244d58
CodyMills <CodysNewGroove@users.noreply.github.com>
2024-08-13 16:07:13 +0100

    Cleaned up Workspace

--- SCRIPTS/health_component.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-@export var MAX_HEALTH := 10.0
-var health : float
-
-func _ready():
-	health = MAX_HEALTH
--- SCRIPTS/hitbox_component.gd
+++
@@ -1,8 +0,0 @@
-class_name HitBoxComponent
-extends Area2D
-
-@export var Damage := 10
-
-func _init() -> void:
-	pass
-	
