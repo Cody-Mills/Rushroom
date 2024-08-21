7489eb74475ed23f1021bb4117fdacc1d0c9e6f2
CodyMills <CodysNewGroove@users.noreply.github.com>
2024-08-21 10:26:00 +0100

    Fixed Animations

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -4,32 +4,19 @@ const Acceleration = 500
 var Max_Speed = 200
 const Friction = 500
 
-@onready var animation = $AnimationPlayer
+@onready var Animation_Player = $AnimationPlayer
+@onready var Animation_Tree = $AnimationTree
+@onready var Animation_State = Animation_Tree.get("parameters/playback")
 
 func _physics_process(delta):
-	DirectionalInput(delta)
-	move_and_slide()
-	DirectionalAnimation()
-
-func DirectionalInput(delta):
 	var Input_Vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	velocity = velocity.move_toward(Input_Vector * Max_Speed, Acceleration * delta)
-
-func DirectionalAnimation():
-	if velocity.length() == 0:
-		animation.stop()
+	
+	if Input_Vector != Vector2.ZERO:
+		Animation_Tree.set("parameters/Idle/blend_position", Input_Vector)
+		Animation_Tree.set("parameters/Walk/blend_position", Input_Vector)
+		Animation_State.travel("Walk")
+		velocity = velocity.move_toward(Input_Vector * Max_Speed, Acceleration * delta)
 	else:
-		var Direction
-		if velocity.x < 0: 
-			$Sprite2D.flip_h = true
-			Direction = "Left"
-		elif velocity.x > 0:
-			$Sprite2D.flip_h = false
-			Direction = "Right"
-		elif velocity.y < 0:
-			$Sprite2D.flip_v = false
-			Direction = "Up"
-		elif velocity.y > 0:
-			$Sprite2D.flip_v = true
-			Direction = "Down"
-		animation.play("Move" + Direction)
+		Animation_State.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, Friction * delta)
+	move_and_slide()
