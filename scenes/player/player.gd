610f2242ef1782b368e2d2de6122896564aaedde
CodyMills <CodysNewGroove@users.noreply.github.com>
2024-09-20 21:14:56 +0100

    New Animations in progreses

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,17 @@
 class_name Player
 extends CharacterBody2D
 
+#Movement
 const Acceleration = 500
 var Walk_Speed = 200
 const Friction = 500
+#Animation
+var last_facing_direction := Vector2(0, -1)
+@onready var animation_tree = $AnimationTree
+
 
 enum{
+	IDLE,
 	MOVE,
 	ATTACK,
 }
@@ -14,25 +20,24 @@ var State = MOVE
 
 func _physics_process(delta):
 	match State:
+		IDLE:
+			IdleState()
 		MOVE:
 			MoveState(delta)
-		ATTACK:
-			AttackState()
+		#ATTACK:
+			#AttackState()
+
+
+func IdleState():
+	velocity = Vector2.ZERO
 
 func MoveState(delta):
 	var Input_Vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	if Input_Vector != Vector2.ZERO:
+		animation_tree.set("parameters/blend_position", velocity)
 		velocity = velocity.move_toward(Input_Vector * Walk_Speed, Acceleration * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, Friction * delta)
+		IdleState()
 	move_and_slide()
 	if Input.is_action_just_pressed("ui_attack"):
 		State = ATTACK
-	
-func AttackState():
-	velocity = Vector2.ZERO
-
-#Goes Back to Idle/Move
-func _on_animation_finished():
-	velocity = Vector2.ZERO
-	State = MOVE
--- scenes/player/player_animation.gd
+++ scenes/player/player_animation.gd
@@ -1,15 +1,8 @@
 extends Node2D
 
 @export var animation_tree : AnimationTree
-@onready var player : Player = get_owner()
+@onready var player : Player = get_owner() #Gets Class Name
 
 var last_facing_direction := Vector2(0, -1)
 
 func _physics_process(delta: float) -> void:
-	var idle = !player.velocity
-	
-	if !idle:
-		last_facing_direction = player.velocity.normalized()
-	
-	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
-	animation_tree.set("parameters/Walk/blend_position", last_facing_direction)
