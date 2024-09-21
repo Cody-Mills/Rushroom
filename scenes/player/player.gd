f86440454b7697e966176be00201ee00aa8b6830
CodyMills <CodysNewGroove@users.noreply.github.com>
2024-09-21 11:14:22 +0100

    New Player character and animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -11,7 +11,6 @@ var last_facing_direction := Vector2(0, -1)
 
 
 enum{
-	IDLE,
 	MOVE,
 	ATTACK,
 }
@@ -20,24 +19,26 @@ var State = MOVE
 
 func _physics_process(delta):
 	match State:
-		IDLE:
-			IdleState()
 		MOVE:
 			MoveState(delta)
-		#ATTACK:
-			#AttackState()
+		ATTACK:
+			AttackState()
 
-
-func IdleState():
-	velocity = Vector2.ZERO
 
 func MoveState(delta):
 	var Input_Vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	SetAnimations()
 	if Input_Vector != Vector2.ZERO:
-		animation_tree.set("parameters/blend_position", velocity)
 		velocity = velocity.move_toward(Input_Vector * Walk_Speed, Acceleration * delta)
 	else:
-		IdleState()
+		velocity = velocity.move_toward(Vector2.ZERO, Friction * delta)
 	move_and_slide()
 	if Input.is_action_just_pressed("ui_attack"):
 		State = ATTACK
+
+func AttackState():
+	print("Attack")
+
+func SetAnimations():
+	animation_tree.set("parameters/Walk/blend_position", velocity)
+	animation_tree.set("parameters/Idle/blend_position", velocity)
