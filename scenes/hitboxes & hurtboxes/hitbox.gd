d21c57c718a876b7602a3c1b120cc1ba274ae4c3
Cody Mills <CodyMills11@hotmail.com>
2024-10-24 14:29:28 +0100

    Fixed Collisions for hit & hurt boxes
    I had to change the monitoring and monitorable variables

--- scenes/hitboxes & hurtboxes/hitbox.gd
+++ scenes/hitboxes & hurtboxes/hitbox.gd
@@ -3,5 +3,5 @@ extends Area2D
 @export var DAMAGE : int = 1
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		print("Damage")
+	if body.has_node("Hurtbox") && has_method("_damage"):
+		print(body)
